73a582c737f06f4a827bb3ab6f989d8279b48263
Aditi Rawal <80158864+aditi272@users.noreply.github.com>
2022-11-19 03:06:28 +0530

    Update dog.js
    made changes in JavaScript code

--- dog.js
+++ dog.js
@@ -1,27 +1,39 @@
-const currentTime = setInterval(function () {
-  const now = new Date();
-  const hour = `${now.getHours()}`.padStart(2, 0);
-  const min = `${now.getMinutes()}`.padStart(2, 0);
-  const sec = `${now.getSeconds()}`.padStart(2, 0);
-  let meridian = `AM`;
-  if (12 <= hour && hour < 17) {
-    meridian = `PM`;
-    document.querySelector(".pics").src =
-      "https://www.do-pal.co.in/wp-content/uploads/2019/03/good-afternoon-cute-dog-images.jpg";
-  } else if (17 <= hour && hour < 20) {
-    meridian = `PM`;
-    document.querySelector(".pics").src =
-      "https://memegenerator.net/img/instances/73720590.jpg";
-  } else if (20 <= hour && hour < 6) {
-    meridian = `PM`;
-    document.querySelector(".pics").src =
-      "https://i.pinimg.com/originals/56/2f/a9/562fa9687cff0023c3fd4f868916d55c.jpg";
-  } else {
-    meridian = `AM`;
-    document.querySelector(".pics").src =
-      "https://i.pinimg.com/originals/3f/0f/e1/3f0fe1df1c1443e9561833c8771d5c14.png";
-  }
-  document.querySelector(
-    ".tick"
-  ).textContent = `${hour}:${min}:${sec} ${meridian}`;
-}, 1000);
+const currentTime = setInterval(()=>{
+            let  a = new Date();
+         let time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+         
+         let date = a.toLocaleDateString();
+        let hours = a.getHours()
+        let min = a.getMinutes();
+    let sec = a.getSeconds();
+        let meridian = "AM";
+    
+     if (12 <= hour && hour < 17) {
+      meridian = `PM`;
+      document.querySelector(".pics").src =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ56ntEFR5XM0m4RP4BTo8jxRTdkzkqMQP3luFtp7O_eWmcUKq4_nveRVpKS472u5VnM0&usqp=CAU";
+    } else if (17 <= hour && hour < 20) {
+      meridian = `PM`;
+      document.querySelector(".pics").src =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVySXLqEZ-gNedSDd3wFQh-pVY665SOvm56g&usqp=CAU";
+    } else if (20 <= hour && hour < 00) {
+      meridian = `PM`;
+      document.querySelector(".pics").src =
+        "https://i.pinimg.com/originals/56/2f/a9/562fa9687cff0023c3fd4f868916d55c.jpg";
+    } else if (00 <= hour && hour <= 5) {
+      meridian = `AM`;
+      document.querySelector(".pics").src =
+        "https://i.pinimg.com/originals/56/2f/a9/562fa9687cff0023c3fd4f868916d55c.jpg";
+    } 
+  else  {
+      meridian = `AM`;
+      document.querySelector(".pics").src =
+        "https://i.pinimg.com/originals/3f/0f/e1/3f0fe1df1c1443e9561833c8771d5c14.png";
+    }
+  
+  
+       document.querySelector(
+      ".tick"
+    ).textContent  =  `${hour}:${min}:${sec} ${meridian} on ${date}`;
+        },1000);
+        
